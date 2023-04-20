252de0fe309888d95d8931f0afdcfecfc88004af
jason1642 <jason1642@gmail.com>
2023-04-20 16:19:09 -0400

    Progress bar filler component

--- src/components/dashboard-meter/gauge-meter/GaugeMeter.tsx
+++ src/components/dashboard-meter/gauge-meter/GaugeMeter.tsx
@@ -2,49 +2,10 @@ import * as React from 'react';
 import styled from 'styled-components';
 import type { labels, value, progressBarColor, titleFontSize, maxValues } from '../types'
 import Container from './Container'
-import { calcRem } from './methods';
 import ProgressBarFiller from './ProgressBarFiller';
-interface RotatingProgressBarProps {
-  percentFilled: number;
-  maxValues: { maxHeight: number, maxWidth: number };
-}
+import ProgressBarCover from './ProgressBarCover';
 
-// Entire progress bar underneath actual filler
-const RotatingProgressBar = styled.div<RotatingProgressBarProps>`
-  position: absolute;
-  top: 0;
-  left: 0;
 
-  width: ${({ maxValues: { maxWidth } }) => calcRem(maxWidth)};
-  height:${({ maxValues: { maxHeight, maxWidth } }) => calcRem(maxWidth)};
-  background: transparent;
-
-  transform: ${({ percentFilled }) => `rotate(${(percentFilled / 100) * 180}deg) translate3d(0,0,0)`};
-  transform-origin: center center;
-  backface-visibility: hidden;
-  transition: all .3s ease-in-out;
-  /* Change percentage filled below - 180deg = 100%, 60def = 30%, ect */
-
-  /* transform: rotate(90deg) translate3d(0,0,0); */
-  &::before {
-    content: "";
-
-    position: absolute;
-    top: 0;
-    left: 0%;
-    z-index: 2;
-    display: flex;
-    width: ${({ maxValues: { maxWidth } }) => calcRem(maxWidth + (maxWidth / 100))};
-  height:${({ maxValues: { maxWidth } }) => calcRem((maxWidth / 2) + (maxWidth / 100))};
-  
-    /* 200w & 100h - Set proportional margins */
-    margin: -1px 0 0 -1px;
-
-    background: #808080;
-
-    border-radius: 50% 50% 50% 50% / 100% 100% 0% 0% ;
-  }      
-`;
 
 
 const Title = styled.div<{titleFontSize: string}>`
@@ -105,7 +66,7 @@ const GaugeMeter: React.FunctionComponent<IMeterProps> = (
           progressBarColor={progressBarColor}
           />
 
-        <RotatingProgressBar
+        <ProgressBarCover
           percentFilled={percentFilled}
           maxValues={{ maxHeight: maxValues.maxWidth / 2, maxWidth: maxValues.maxWidth }}
         />
--- src/components/dashboard-meter/gauge-meter/ProgressBarCover.tsx
+++ src/components/dashboard-meter/gauge-meter/ProgressBarCover.tsx
@@ -1,11 +1,64 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import { calcRem } from './methods';
+
+
+
+
+
+interface RotatingProgressBarProps {
+    percentFilled: number;
+    maxValues: { maxHeight: number, maxWidth: number };
+  }
+// Entire progress bar underneath actual filler
+const RotatingProgressBar = styled.div<RotatingProgressBarProps>`
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: ${({ maxValues: { maxWidth } }) => calcRem(maxWidth)};
+  height:${({ maxValues: { maxHeight, maxWidth } }) => calcRem(maxWidth)};
+  background: transparent;
+
+  transform: ${({ percentFilled }) => `rotate(${(percentFilled / 100) * 180}deg) translate3d(0,0,0)`};
+  transform-origin: center center;
+  backface-visibility: hidden;
+  transition: all .3s ease-in-out;
+  /* Change percentage filled below - 180deg = 100%, 60def = 30%, ect */
+
+  /* transform: rotate(90deg) translate3d(0,0,0); */
+  &::before {
+    content: "";
+
+    position: absolute;
+    top: 0;
+    left: 0%;
+    z-index: 2;
+    display: flex;
+    width: ${({ maxValues: { maxWidth } }) => calcRem(maxWidth + (maxWidth / 100))};
+  height:${({ maxValues: { maxWidth } }) => calcRem((maxWidth / 2) + (maxWidth / 100))};
+  
+    /* 200w & 100h - Set proportional margins */
+    margin: -1px 0 0 -1px;
+
+    background: #808080;
+
+    border-radius: 50% 50% 50% 50% / 100% 100% 0% 0% ;
+  }      
+`;
+
 
 interface IProgressBarCoverProps {
+    percentFilled: number;
+    maxValues: { maxHeight: number, maxWidth: number };
 }
 
-const ProgressBarCover: React.FunctionComponent<IProgressBarCoverProps> = (props) => {
+const ProgressBarCover: React.FunctionComponent<IProgressBarCoverProps> = ({percentFilled, maxValues}) => {
   return (
-    <></>
+    <RotatingProgressBar
+    percentFilled={percentFilled}
+    maxValues={{ maxHeight: maxValues.maxWidth / 2, maxWidth: maxValues.maxWidth }}
+  />
   );
 };
 
